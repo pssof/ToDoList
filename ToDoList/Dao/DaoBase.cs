601bf0d43e6dc7f8ccff5c6e645ffe25025b96dc
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-01 00:09:55 -0400

    Criado a classe DataContext, DaoBase, inseridos os comandos de abertura e encerramento de conexão com o banco.

---
+++ ToDoList/Dao/DaoBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToDoList.Context;
+
+namespace ToDoList.Dao
+{
+    public class DaoBase
+    {
+        protected readonly DataContext banco;
+
+        public DaoBase()
+        {
+            banco = new DataContext();
+        }
+
+        public void Dispose()
+        {
+            banco.Dispose();
+        }
+    }
+}
