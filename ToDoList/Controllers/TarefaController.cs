f957819d1fb4509ec6b7fd3ea067ba14bbf3440e
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-08 21:50:21 -0400

    Inseridas as informações na Controller TarefaController

--- ToDoList/Controllers/TarefaController.cs
+++ ToDoList/Controllers/TarefaController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Dao;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -11,8 +13,41 @@ namespace ToDoList.Controllers
         // GET: Tarefa
         public ActionResult Index()
         {
-            
+            var tarefas = new TarefaDao().Buscar();
             return View();
+        }
+        public ActionResult Novo()
+        {
+            ViewBag.Tarefas = new SelectList(new TarefaDao().Buscar().ToDictionary(t => t.TarefaID, t => t.NomeTarefa), "Key", "Value");
+            return View();
+        }
+        public ActionResult Editar(int id)
+        {
+            ViewBag.Tarefas = new SelectList(new TarefaDao().Buscar().ToDictionary(t => t.TarefaID, t => t.NomeTarefa), "Key", "Value");
+            var tarefa = new TarefaDao().Buscar(id);
+            return View(tarefa);
+        }
+        [HttpPost]
+        public ActionResult Editar(Tarefa objTarefa)
+        {
+            new TarefaDao().Alterar(objTarefa);
+            return RedirectToAction("Index", "Tarefa");
+        }
+        [HttpPost]
+        public ActionResult Novo(Tarefa objTarefa)
+        {
+            new TarefaDao().Gravar(objTarefa);
+            return RedirectToAction("Index", "Tarefa");
+        }
+        public ActionResult Detalhe(int id)
+        {
+            Tarefa tarefa = new TarefaDao().Buscar(id);
+            return View(tarefa);
+        }
+        public ActionResult Excluir(int id)
+        {
+            new TarefaDao().Excluir(id);
+            return RedirectToAction("Index", "Tarefa");
         }
     }
 }
