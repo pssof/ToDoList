d9c03ca7486a632f2ca6b6d93b42479ad482652b
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-11 23:43:34 -0400

    Criado a Model Endereco

---
+++ ToDoList/Models/Contato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Contato
+    {
+        [Key]
+        public int ContatoID { get; set; }
+
+        [MaxLength(60)]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Nome do Contato")]
+        [Required(ErrorMessage = "O nome do contato é obrigatório!")]
+        public string NomeContato { get; set; }
+
+        [MaxLength(14)]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Telefone Fixo")]
+        public string TelefoneFixoContato { get; set; }
+
+        [MaxLength(14)]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Celular")]
+        public string CelularContato { get; set; }
+
+        [MaxLength(30)]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "E-mail")]
+        public string EmailContato { get; set; }
+
+
+    }
+}
