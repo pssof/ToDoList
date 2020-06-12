644f378b9034bcd71573f7076be9b9c38e857edb
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-12 01:03:24 -0400

    Inseridos os códigos da EnderecoController.

---
+++ ToDoList/Controllers/EnderecoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Dao;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        // GET: Endereco
+        public ActionResult Index()
+        {
+            var listaEnderecos = new EnderecoDao().Listar();
+
+            return View(listaEnderecos);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Novo(Endereco objEndereco)
+        {
+            new EnderecoDao().Gravar(objEndereco);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Endereco objEndereco = new EnderecoDao().Buscar(id);
+            return View(objEndereco);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Endereco objEndereco)
+        {
+            new EnderecoDao().Alterar(objEndereco);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detalhar(int id)
+        {
+            Endereco objEndereco = new EnderecoDao().Buscar(id);
+            return View(objEndereco);
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            new EnderecoDao().Excluir(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
