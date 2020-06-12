88c6ac6c9aca915de38440457b44d37c673aafd2
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-12 00:37:12 -0400

    Inseridos os códigos na ContatoController.

--- ToDoList/Controllers/ContatoController.cs
+++ ToDoList/Controllers/ContatoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Dao;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -11,8 +13,45 @@ namespace ToDoList.Controllers
         // GET: Contato
         public ActionResult Index()
         {
-            v
+            var listaContatos = new ContatoDao().Listar();
+            return View(listaContatos);
+        }
+
+        public ActionResult Novo()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Novo(Contato objContato)
+        {
+            new ContatoDao().Gravar(objContato);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Contato objContato = new ContatoDao().Buscar(id);
+            return View(objContato);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Contato objContato)
+        {
+            new ContatoDao().Alterar(objContato);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detalhar(int id)
+        {
+            Contato objContato = new ContatoDao().Buscar(id);
+            return View(objContato);
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            new ContatoDao().Excluir(id);
+            return RedirectToAction("Index");
         }
     }
 }
