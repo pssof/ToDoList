10b720779a8b9460e2365530599a08e036f13a22
Paulo Sérgio Filho <paulo.o.sergio2@gmail.com>
2020-06-12 00:48:41 -0400

    Criado EndereçoDao e inseridos os códigos da Classe.

---
+++ ToDoList/Dao/EnderecoDao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ToDoList.Models;
+
+namespace ToDoList.Dao
+{
+    public class EnderecoDao :DaoBase
+    {
+        public void Gravar(Endereco objEndereco)
+        {
+            banco.Enderecos.Add(objEndereco);
+            banco.SaveChanges();
+        }
+
+        public void Alterar(Endereco objEndereco)
+        {
+            var entry = banco.Entry(objEndereco);
+            banco.Set<Endereco>().Attach(objEndereco);
+            entry.State = EntityState.Modified;
+            banco.SaveChanges();
+        }
+
+        public Endereco Buscar(int id)
+        {
+            return banco.Enderecos.FirstOrDefault(e => e.EnderecoID == id);
+        }
+
+        public void Excluir(int id)
+        {
+            Endereco obj = Buscar(id);
+            banco.Enderecos.Remove(obj);
+            banco.SaveChanges();
+        }
+
+        public IEnumerable<Endereco> Listar()
+        {
+            return banco.Enderecos.ToList();
+        }
+
+    }
+}
